53962a4a77b90cfbf27f4f090ee14fb8eb78f82e
Smaug6739 <veauville.raphael@gmail.com>
2021-10-25 20:22:09 +0200

    Fix typing

--- typing/typescript/interfaces.d.ts
+++ typing/typescript/interfaces.d.ts
@@ -1,10 +1,10 @@
 export interface IOptions {
-    caseInsensitive: boolean;
-    required: boolean;
-    multipleReplaces: boolean;
-    skip: number;
-    maxReplaces: number;
+  caseInsensitive?: boolean;
+  required?: boolean;
+  multipleReplaces?: boolean;
+  skip?: number;
+  maxReplaces?: number;
 }
 export interface IReplace {
-    [index: string]: string;
+  [index: string]: string;
 }
